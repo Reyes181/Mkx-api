3ff752ddf2801e9a80c4f26489b84312bf06633b
Emilio R <reyesdasix181@gmail.com>
2018-08-31 01:02:38 +0000

    add mlab database to index

--- models/index.js
+++ models/index.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose');
 
 mongoose.set('debug', true);
 
-mongoose.connect("mongodb://localhost/mkx_api");
+// mongoose.connect("mongodb://localhost/mkx_api");
+mongoose.connect("mongodb://Emilio:bairdfan45@ds239682.mlab.com:39682/er_mkxapi");
 
 mongoose.Promise = Promise;
 
